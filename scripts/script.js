66cdc0913e8792d05d3b1eb689c197e39877e84e
CamSund02 <60443745+CamSund02@users.noreply.github.com>
2023-10-24 14:04:05 -0500

    Sticky Nav
    Added javascript and css for sitcky header within --NEW-- section found on css. Added id="header" to header sections.

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,23 @@ toggleBtn.onclick = function () {
     ? 'fa-solid fa-xmark'
     : 'fa-solid fa-bars'
 }
+
+//NEW!!
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {stickyfunction()};
+
+// Get the header
+var header = document.getElementById("header");
+
+// Get the offset position of the header
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyfunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky")
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+//NEW!!
